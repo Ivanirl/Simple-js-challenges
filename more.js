62e4e576e69d144798e917446eb7729744a490df
Ivan <ivanezeh12@gmail.com>
2023-05-03 20:39:58 +0100

    3

--- more.js
+++ more.js
@@ -2,9 +2,15 @@ const caro = document.querySelector(".carousel")
 
 let isdragging = false
 
+function brag(){
+    isdragging = true
+}
+
 function drag(e){
     if(!isdragging) return //this means if isdragging is false return from here
     caro.scrollLeft = e.pageX
 }
 
+
+caro.addEventListener("mousedown", brag)
 caro.addEventListener("mousemove", drag)
