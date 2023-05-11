97f46db01161b5d03b8dc4176b84292b53bea1f5
Ivan <ivanezeh12@gmail.com>
2023-05-11 10:33:49 +0100

    ano

--- draggable list/drag.js
+++ draggable list/drag.js
@@ -1,13 +1,14 @@
-const op = document.querySelector(".draggable-list")
+const op = document.querySelector(".draggable-list .item")
 
 const hey = op.querySelectorAll(".item")
 
 hey.addEventListener("click", () => {
-    op.innerHTML = `
+    op.innerHTML += `
     <div>
     <h1>
     I HATE YOU
     </h1>
     </div>
     `
+    console.log("big boy pants")
 })
