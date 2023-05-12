e778c735bad3c3e47dcb829ea24dc7c677b9a039
Ivan <ivanezeh12@gmail.com>
2023-05-12 12:47:41 +0100

    2

--- draggable list/drag.js
+++ draggable list/drag.js
@@ -1,12 +1,32 @@
-const op = document.querySelector(".draggable-list")
+const sorta = document.querySelector(".draggable-list")
 
+const booton = document.querySelector(".draggable-list button")
 const item = document.querySelectorAll(".item")
+const bod = document.querySelector("body")
 
 item.forEach(item=>{
-    item.addEventListener("click", ()=>{
-        op.innerHTML += `<button>Howdy</button>`
+    item.addEventListener("dragstart", ()=>{
+        setTimeout(()=> item.classList.add("dragging"),0)
     })
+    item.addEventListener("dragend", ()=> {item.classList.remove("dragging")})
 })
 
 
-    console.log("big boy pants")
+const initSortableList =  (e) => {
+    e.preventDefault()
+
+    const DraggingItem = sorta.querySelector(".dragging")
+
+    const siblings = [...document.querySelectorAll(".item:not(.dragging)")]
+
+    let nextSibling = siblings.find(sibling => {
+        return e.clientY <= sibling.offsetTop + sibling.offsetHeight / 2
+    })
+
+    sorta.insertBefore(DraggingItem, nextSibling)
+}
+
+sorta.addEventListener("dragover", initSortableList)
+sorta.addEventListener("dragenter", e => e.preventDefault())
+
+console.log("big boy pants")
