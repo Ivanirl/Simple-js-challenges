2e3fbe714190b722989402422e2cb96c838b8b9d
Ivan <ivanezeh12@gmail.com>
2023-10-04 07:33:35 +0100

    one

--- Questionaire/Quest.js
+++ Questionaire/Quest.js
@@ -1,13 +1,47 @@
 // if active page = 1 change class of bar to the 25% holder
 
-const bodice = document.querySelector(".bodice")
+const bodice = document.querySelector(".bodice");
+const jace = document.querySelector(".jace");
 
-const numba = 0
+let numba = 0;
 
-//bodice.classList = "noshow"
+function next() {
+  numba += 1;
+  const pop = "page" + numba;
+  if (numba > 4) {
+    console.log("end of the road broddie!");
+    numba = 4
+  } else {
+    bodice.className = pop;
+    console.log(numba);
+  }
+  progress()
+}
 
-function next(){
-    const gugu = numba + 1
-    const pop = "page"+gugu
-    bodice.className = pop
+function back() {
+  numba -= 1;
+  const pop = "page" + numba;
+  if (numba < 0) {
+    console.log("end of the road broddie!");
+    numba = 0
+  } else {
+    bodice.className = pop;
+    console.log(numba);
+  }
+}
+
+function progress(){
+    if(numba = 0){
+        console.log("oneeeeeeeeeeeeeeeee")
+    }else if(numba = 1){
+
+    }else if(numba = 2){
+
+    }else if(numba = 3){
+
+    }else if(numba = 4){
+        
+    }else{
+        console.log("poppycock!")
+    }
 }
