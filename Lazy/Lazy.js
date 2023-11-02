4a3308342dfcc7be44042b7a827c58887eaab664
Ivan <ivanezeh12@gmail.com>
2023-11-02 08:52:41 +0100

    one

--- Lazy/Lazy.js
+++
@@ -1,9 +0,0 @@
-const pop = document.querySelector('#1')
-
-function scroll(){
-    window.addEventListener(scroll, ()=>{
-        pop.className = "active"
-    })
-}
-//does nit work ata ll
-scroll()
